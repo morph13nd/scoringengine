322bea3bcc2d3bbaeb1293f60a26c144e5eb8188
Arian-D <ArianDehghani@protonmail.com>
2020-08-31 21:47:53 -0700

    Added 2 simple functions for the http module and a template for main

---
+++ protocols/http.py
@@ -0,0 +1,17 @@
+from requests import get
+
+def http_status(url: str, status_codes: [int]):
+    """
+    Check whether the url returns one of the given status codes
+    """
+    try:
+        code = get(url).status_code
+        return code in status_codes
+    except:
+        return False
+
+def http_contains(url: str, keywords: [str], case_sensitive=True):
+    """
+    Returns true upon finding the keyword in the given page
+    """
+    pass
